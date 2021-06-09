6e6f7b10c3aae1cf88decec3844fc805ae4414b3
hjt523 <harry.tindale1997@gmail.com>
2021-06-09 13:43:43 +0100

    Basic app.pys sorted, not yet functional

--- service1/app.py
+++ service1/app.py
@@ -1,1 +1,30 @@
+from flask import Flask, redirect, render_template, request,url_for 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+import requests
+
+
+app = Flask(__name__) # Creating Flask Object
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db" # Set the connection string to connect to the database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.config['SECRET_KEY'] = ""
+
+db = SQLAlchemy(app) # create SQLALchemy object
+
+@app.route("/", methods =["GET","POST"]) # main Page
+def main():
+    #if request.method == "POST" :
+        aesthetic = requests.get("http://service2:5000/service2").text
+        subject = requests.get("http://service3:5000/service3").text
+        doodle = requests.post("http://service4:5000/service4", aesthetic = aesthetic, subject = subject).text
+        print (doodle)
+        return render_template("main.html", doodle = doodle )
+    #return render_template("main.html", doodle = doodle)
+
+if __name__=='__main__':
+    app.run(host = "0.0.0.0",port=5000, debug=True)
+
 
