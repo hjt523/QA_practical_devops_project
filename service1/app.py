9b30a2cec25cc4e30e77b977c9fe585f06bf7a9c
hjt523 <harry.tindale1997@gmail.com>
2021-06-10 13:52:26 +0100

    Now utilises mysql database

--- service1/app.py
+++ service1/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, render_template, request,url_for 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
 import requests
 
 
@@ -10,19 +11,33 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = ""
 
 db = SQLAlchemy(app) # create SQLALchemy object
+class prior(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    doodle = db.Column(db.String(60),nullable=False)
+
+db.drop_all()
+db.create_all()
 
 @app.route("/", methods =["GET","POST"]) # main Page
 def main():
     #doodle = "Example output"
     #if request.method == "POST" :
     aes = requests.get("http://service2:5000/service2").text
-    print(aes)
+    #print(aes)
     sub = requests.get("http://service3:5000/service3").text
     
     doodle = requests.post("http://service4:5000/service4", data= aes + "_"+sub).text
-    print (doodle)
-    return render_template("main.html", doodle = doodle )
+    
+    doodlesprior = prior.query.order_by(desc(prior.id)).limit(5).all()
+
+    storedoodle = prior(doodle = doodle)
+    db.session.add(storedoodle)
+    db.session.commit()
+
+    return render_template("main.html", doodle = doodle , doodlesprior = doodlesprior)
     #return render_template("main.html", doodle = doodle)
+
+
 
 if __name__=='__main__':
     app.run(host = "0.0.0.0",port=5000, debug=True)
