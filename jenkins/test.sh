a6dcc095da81a747f6b4ca1ad91b5cc80d3abb7e
hjt523 <81659044+hjt523@users.noreply.github.com>
2021-06-11 16:49:35 +0100

    Create test.sh

---
+++ jenkins/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# install Requirements
+
+sudo apt update
+sudo apt install python3-venv python3-pip
+
+python3 -m venv venv
+source venv/bin/activate
+
+pip3 install pytest
+
+# Service1
+
+cd service1
+python3 -m pytest --cov=app
+cd .. 
+
+# Service2
+
+cd service2
+python3 -m pytest --cov=app
+cd .. 
+
+# Service3
+
+cd service3
+python3 -m pytest --cov=app
+cd .. 
+
+# Service4
+
+cd service4
+python3 -m pytest --cov=app
+cd .. 
