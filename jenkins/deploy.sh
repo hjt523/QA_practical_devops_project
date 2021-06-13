ed0296cd154c083e71757ba9befb81a6b669bf3d
Harry <harry.tindale1997@gmail.com>
2021-06-13 19:17:23 +0000

    Setup Deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# Copying yaml to manager
+scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager:/home/jenkins/docker-compose.yaml
+
+# Deploy Stack
+ssh -i ~/.ssh/ansible_id_rsa jenkins@manager << EOF
+    export DATABASE_URI=${DATABASE_URI}
+    docker stack deploy --compose-file docker-compose.yaml doodlestack
+EOF
