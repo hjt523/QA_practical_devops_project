a58a1fe385674c2e7ae68965893aba3eb087af38
hjt523 <81659044+hjt523@users.noreply.github.com>
2021-06-13 20:57:29 +0100

    Update deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -4,7 +4,7 @@
 scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager:/home/jenkins/docker-compose.yaml
 
 # Deploy Stack
-ssh -i ~/.ssh/ansible_id_rsa jenkins@manager << EOF
+ssh -tt -i ~/.ssh/ansible_id_rsa jenkins@manager << EOF
     export DATABASE_URI=${DATABASE_URI}
     docker stack deploy --compose-file docker-compose.yaml doodlestack
 EOF
